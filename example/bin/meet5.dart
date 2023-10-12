dc75d0b70d1007f8910ae3744d7b387b9ef402a9
deny <deny@google.com>
2023-10-12 21:33:53 +0700

    .

---
+++ example/bin/meet5.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+
+// Function to get products using DIO
+Future<List<dynamic>> getProducts() async {
+  try {
+    final dio = Dio();
+
+    final response = await dio.get(
+      'https://capekngoding.com/demo/api/products',
+      options: Options(
+        sendTimeout: Duration(seconds: 100),
+        receiveTimeout: Duration(seconds: 100),
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final jsonData = response.data;
+      final List<dynamic> products = jsonData['data'];
+      return products;
+    } else {
+      throw Exception(
+          'Failed to get products. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw DioException(requestOptions: RequestOptions());
+  }
+}
+
+void main() async {
+  /*
+  1. 1 tab identation
+  2. Alur-nya bisa membingungkan
+  */
+  /*
+  var products = await getProducts();
+  print("Berhasil menampilkan product");
+  */
+
+  List products = await getProducts();
+  print(products);
+}
